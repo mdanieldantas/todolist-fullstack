64ea5509e0c532e1a4f7a64b971a2908125d59ea
M Daniel  Dantas <contatomarcosdgomes@gmail.com>
2024-11-13 09:41:56 -0300

    adiciona suporte a variáveis de ambiente e atualiza mensagem de inicialização do servidor

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,10 @@
 const app = require('./app');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3333;
 
 app.listen(3333, () => {
-    console.log("Server running on port 3333");
+    console.log(`Server is running on port ${PORT}`);
 });
 
 
