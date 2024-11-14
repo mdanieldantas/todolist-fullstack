46679a55c55c9eb95b346c617ae3993bcc148ba4
M Daniel  Dantas <contatomarcosdgomes@gmail.com>
2024-11-14 13:00:47 -0300

    remove logs de variáveis de ambiente na conexão com MySQL

--- backend/src/models/connection.js
+++ backend/src/models/connection.js
@@ -1,10 +1,6 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-console.log(process.env.MYSQL_HOST);
-console.log(process.env.MYSQL_USER);
-console.log(process.env.MYSQL_PASSWORD);
-console.log(process.env.MYSQL_DB);
 
 
 
@@ -12,7 +8,9 @@ const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DB,
+  database: process.env.MYSQL_DB
+  
 });
 
 module.exports = connection;
+
